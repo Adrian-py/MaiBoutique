b1a076cd14128147d33aa9a9adaabafcadd2002f
Adrian-py <62276355+Adrian-py@users.noreply.github.com>
2022-12-30 23:50:31 +0700

    Fix: add 'stock' attribute for products table

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -19,6 +19,7 @@ class ProductFactory extends Factory
             "price" => $this->faker->numberBetween(50000, 10000000),
             "description" => $this->faker->text(),
             "image" => "images/" . "shirt-" . rand(1, 3) . ".jpg",
+            "stock" => $this->faker->numberBetween(5, 20),
         ];
 
         $product["slug"] = Str::slug($product["name"]);
--- database/migrations/2022_12_08_130247_create_products_table.php
+++ database/migrations/2022_12_08_130247_create_products_table.php
@@ -20,6 +20,7 @@ class CreateProductsTable extends Migration
             $table->integer("price");
             $table->string("description");
             $table->string("image");
+            $table->integer("stock");
             $table->timestamps();
         });
     }
